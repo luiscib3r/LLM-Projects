b23e4d74b52aaa1664bb34a724a2d6dfd9941a86
Luis Correa <luisciber640@gmail.com>
2024-04-07 23:30:48 -0300

    feat(sonia): add submit button in chat window

--- sonia/src/app/chat/[chatId]/_components/chat_window/index.tsx
+++ sonia/src/app/chat/[chatId]/_components/chat_window/index.tsx
@@ -1,9 +1,11 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { Message } from "ai";
 import { useChat } from "ai/react";
+import { Send } from "lucide-react";
 import { useEffect, useRef } from "react";
 import { MessageTile } from "./message_tile";
 
@@ -39,13 +41,17 @@ export const ChatWindow = ({ chatId, initialMessages }: ChatWindowProps) => {
         <div ref={scrollRef} />
       </ScrollArea>
       <div className="w-full text-center my-2 mx-auto px-4 md:max-w-[80%] lg:max-w-[70%] 2xl:max-w-[50%]">
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit} className="flex">
           <Input
             className="bg-neutral-800 border-orange-800 focus:border-orange-500 h-12"
             placeholder="How can I help you?"
             value={input}
             onChange={handleInputChange}
           />
+          <div className="w-2" />
+          <Button type="submit" className="h-12 bg-orange-600 hover:bg-orange-500">
+            <Send />
+          </Button>
         </form>
       </div>
     </div>
